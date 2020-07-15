e6a5d112699a5d261feba564a13558d630c33c0a
garuav <gbgaurav461@gmail.com>
2020-07-15 21:29:51 +0530

    added popover for success and failure and added the email validation

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,6 +12,11 @@ export class ContactComponent implements OnInit {
   contactForm: FormGroup;
   loginData;
   formSubmitted: boolean;
+  response = {
+    isSuccess: false,
+    message: '',
+    isSubmitted: false
+  };
   constructor(private fb: FormBuilder, private commonService: CommonService) { }
 
   ngOnInit(): void {
@@ -22,7 +27,7 @@ export class ContactComponent implements OnInit {
   createForm() {
     this.contactForm = this.fb.group({
       name: [(this.loginData && this.loginData.displayName) ? this.loginData.displayName : '', [Validators.required]],
-      email: [(this.loginData && this.loginData.email) ? this.loginData.email : '', [Validators.required]],
+      email: [(this.loginData && this.loginData.email) ? this.loginData.email : '', [Validators.required, Validators.email]],
       description: ['', [Validators.required]]
     });
   }
@@ -39,18 +44,41 @@ export class ContactComponent implements OnInit {
         }
         this.commonService.saveContactData(contactValue).then(res => {
         // console.log('response from save contact = ', res);
+        this.popoverData(false, true);
         this.formSubmitted = false;
         this.createForm();
-
+        this.popoverData(true);
       }).catch(error => {
         console.log('error from save contact = ', error);
-
-      })
+        this.popoverData(false, false);
+      });
       }, error => {
         console.log('error send notification = ', error);
 
       });
     }
   }
-
+  popoverData(clearData, isSuccess?) {
+    if (clearData) {
+      setTimeout(() => {
+        this.response = {
+          isSuccess: false,
+          message: '',
+          isSubmitted: false
+        };
+      }, 2000);
+    } else if (isSuccess) {
+      this.response = {
+        isSuccess: true,
+        message: 'Successfully submitted',
+        isSubmitted: true
+      };
+    } else {
+      this.response = {
+        isSuccess: false,
+        message: 'Something went wrong! Please try again later.',
+        isSubmitted: true
+      };
+    }
+  }
 }
